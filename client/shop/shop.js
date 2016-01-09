08fdcdff889a2dcb760e14a4d7518808faf48d40
Phil Jay Kwon <phjaykwon93@gmail.com>
2016-01-08 16:53:15 -0800

    remove autopublish, setup publish, subscribe

--- client/shop/shop.js
+++ client/shop/shop.js
@@ -5,19 +5,37 @@ Template.homeContent.helpers({
   }
 });
 
+Template.newArrivals.onCreated(function() {
+	//this.subscribe('items', 'Women', null, 1);
+	this.subscribe('items', 'Women', null);
+	this.subscribe('allItems')
+	//this.subscribe('allItems')
+})
 
 console.log('Number of items' + Items.find().count())
 Template.newArrivals.helpers({
 	/* fetch clothes less than 15 days old */
 	name: 'New Arrivals',
-	coverItem: Items.findOne({category: 'Women'}, { sort: { createdAt: -1 }, limit: 1 }),
-	items: Items.find({category: 'Women'},  { sort: { createdAt: -1 }, limit: 10 })
+	cov: Items.findOne({}),
+	coverItem: function() {
+		/* http://dweldon.silvrback.com/guards */
+		var one = Items.findOne({category: 'Women'}, { sort: { createdAt: -1 }})
+		return one && one.photos[0]
+	},
+	items: Items.find({category: 'Women'},  { sort: { createdAt: -1 }})
 })
 
+Template.women.onCreated(function() {
+	this.subscribe('items', 'Women', null, 0);
+})
 Template.women.helpers({
 	/* fetch clothes less than 15 days old */
 
 	items: Items.find({ category: 'Women'})
+})
+
+Template.men.onCreated(function() {
+	this.subscribe('items', 'Men', null, 0);
 })
 
 Template.men.helpers({
