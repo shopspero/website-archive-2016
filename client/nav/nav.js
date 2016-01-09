173049a0f1e050c40c2cdf930376b4646921b43f
phjaykwon <phjaykwon93@gmail.com>
2016-01-09 02:34:35 -0800

    added login

--- client/nav/nav.js
+++ client/nav/nav.js
@@ -1,9 +1,19 @@
 
 Template.mainNav.onCreated(function() {
 	this.subscribe('categories')
+
 })
 
 
 Template.mainNav.helpers({
   categories: Categories.find({})
+
 });
+
+Template.mainNav.events({
+	'click .elem': function() {
+		console.log("LOL???")
+   		$(".dropdown-menu").visibility = "invisible" // not working
+	},
+
+})
--- client/shop/shop.js
+++ client/shop/shop.js
@@ -1,4 +1,4 @@
-
+/* default crap.. use session */
 Template.homeContent.helpers({
   counter: function () {
     return Session.get('counter');
@@ -7,91 +7,39 @@ Template.homeContent.helpers({
 
 Template.shopContent.onCreated(function() {
 	this.category = Template.currentData().category
-	this.subcategory = Template.currentData().subcategoryName
-
+	this.subcategory = (Template.currentData().subcategory && Template.currentData().subcategory.name) || ''
 	this.subscribe('allItems')
 	//this.subscribe('items', this.category, this.subcategory)
-
 })
 
 
 
 Template.shopContent.helpers({
-	namae: function() {
+	name: function() {
 		return Template.currentData().category.name
 	},
-	subcategoryNamed: function() {
-		return Template.currentData().subcategoryName
+	subcategoryName: function() {
+		return (Template.currentData().subcategory && ('- ' + Template.currentData().subcategory.name)) || ''
 	},
-	yo: function() {
-
-		if (Template.currentData().subcategoryName) {
-			return Items.find({category: Template.currentData().category.name, subcategories: Template.currentData().subcategoryName }).count()
+	coverItem: function() {
+		/* http://dweldon.silvrback.com/guards */
+		var one;
+		if (Template.currentData().subcategory) {
+			one = Items.findOne({category: Template.currentData().category.name, subcategories: Template.currentData().subcategory.query }, { sort: { createdAt: -1 }})
 		} else {
-			return Items.find({category: Template.currentData().category.name }).count()
+			one =  Items.findOne({category: Template.currentData().category.name }, { sort: { createdAt: -1 }})
+		}
+		//var one = Items.findOne({category: Template.currentData().category.name}, { sort: { createdAt: -1 }})
+		return one && one.photos[0]
+	},
+	items: function() {
+
+		if (Template.currentData().subcategory) {
+			return Items.find({category: Template.currentData().category.name, subcategories: Template.currentData().subcategory.query }, { sort: { createdAt: -1 }})
+		} else {
+			return Items.find({category: Template.currentData().category.name }, { sort: { createdAt: -1 }})
 		}
 	}
 
 })
-
-
-Template.newArrivals.onCreated(function() {
-	//this.subscribe('items', 'Women', null, 1);
-	this.subscribe('items', 'Women', null);
-	this.subscribe('allItems')
-	//this.subscribe('allItems')
-})
-
-console.log('Number of items' + Items.find().count())
-Template.newArrivals.helpers({
-	/* fetch clothes less than 15 days old */
-	name: 'New Arrivals',
-	cov: Items.findOne({}),
-	coverItem: function() {
-		/* http://dweldon.silvrback.com/guards */
-		var one = Items.findOne({category: 'Women'}, { sort: { createdAt: -1 }})
-		return one && one.photos[0]
-	},
-	items: Items.find({category: 'Women'},  { sort: { createdAt: -1 }})
-})
-
-Template.women.onCreated(function() {
-	this.subscribe('items', 'Women', null, 0);
-})
-Template.women.helpers({
-	subcategory: function() {
-		var router = Router.current();
-		return router && router.params.query.subcategory
-	},
-	items: Items.find({ category: 'Women'})
-})
-
-Template.women.onRendered(function() {
-	this.subcategory = Router.current().params
-})
-
-Template.men.onCreated(function() {
-	this.subscribe('items', 'Men', null, 0);
-})
-
-Template.men.helpers({
-	/* fetch clothes less than 15 days old */
-	items: Items.find({ category: 'Men'})
-})
-
-Template.accessories.helpers({
-	/* fetch clothes less than 15 days old */
-	items: Items.find({ category: 'Accessories'})
-})
-
-Template.artwork.helpers({
-	/* fetch clothes less than 15 days old */
-	items: Items.find({})
-})
-
-Template.all.helpers({
-	/* fetch clothes less than 15 days old */
-	items: Items.find({})
-})
-
 
