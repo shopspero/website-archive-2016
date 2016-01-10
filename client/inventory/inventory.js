4626003cbf8d79c02638025a1933a57e8caea9ec
phjaykwon <phjaykwon93@gmail.com>
2016-01-09 18:49:35 -0800

    adding inventory logic

---
+++ client/inventory/inventory.js
@@ -0,0 +1,46 @@
+Template.addInventory.onCreated(function() {
+	this.subscribe("categories")
+})
+
+Template.addInventory.helpers({
+	categories: Categories.find({}),
+	subcategories: function() {
+		return Session.get('subcategories') || []
+	}
+})
+Template.addInventory.events({
+	"submit .new-inventory": function (event) {
+		// Prevent default browser form submit
+		event.preventDefault();
+
+		// Get value from form element
+		var text = event.target.name.value;
+		console.log(text)
+
+		var regex  = /^\d+(?:\.\d{0,2})$/;
+		if (regex.test(event.target.price.value)) {
+			alert('good')
+		} else {
+			alert('bad')
+		}
+		// Insert a task into the collection
+		/*
+		Tasks.insert({
+			text: text,
+			createdAt: new Date() // current time
+		}); */
+
+		// Clear form
+		event.target.name.value = "done";
+	},
+	"change #category-select": function(event){
+        var categoryId = event.target.value
+        Session.set('subcategories', Categories.findOne(categoryId).subcategories)
+        if (Categories.findOne(categoryId).subcategories.length > 0) {
+        	console.log('yasss')
+        } else {
+        	console.log('noo')
+        }
+        
+     }
+});
